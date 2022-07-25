d96eb8726e166e543a2edabb1a923cf66b9048b7
xuzt <xuzt@gillion.com.cn>
2022-07-25 09:17:14 +0800

    task#759452  修改接口

---
+++ share-auth/share-auth-manage/share-auth-service/src/main/java/com/share/auth/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.share.auth.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xuzt <xuzt@gillion.com.cn>
+ * @date 2022/7/21
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+//    @RequestMapping("/join")
+//    public ResultHelper<?> test() {
+//        List<SysRole> sysRoleList = QSysRole.sysRole
+//                .select(QSysRole.sysRoleId, QSysRole.roleName)
+//                .execute();
+//        for (SysRole sysRole : sysRoleList) {
+//            List<SysResource> sysResourceList = QSysResource.sysResource
+//                    .select(QSysResource.sysResource.fieldContainer())
+//                    .
+//        }
+//    }
+}
