f60d334cccd718d276755b92e18f453316087c2f
xuzt <xuzt@gillion.com.cn>
2022-07-25 15:13:41 +0800

    task#759452  修改模型

---
+++ share-auth/share-auth-manage/share-auth-service/src/main/java/com/share/auth/model/entity/SysPublishConfig.java
@@ -0,0 +1,107 @@
+package com.share.auth.model.entity;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.gillion.ds.entity.base.BaseModel;
+import com.gillion.ec.core.annotations.Generator;
+import com.gillion.ec.core.utils.Long2String;
+import com.gillion.ec.core.utils.String2Long;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+
+/**
+ * @author DaoServiceGenerator
+ */
+@SuppressWarnings("JpaDataSourceORMInspection")
+        @EqualsAndHashCode(callSuper = true)
+    @Data
+    @Entity
+@Table(name = "sys_publish_config")
+public class SysPublishConfig extends BaseModel implements Serializable{
+private static final long serialVersionUID=1;
+
+    /**id*/
+    @Id
+    @Column(name = "sys_publish_config_id")
+    @JsonSerialize(using = Long2String.class)
+    @JsonDeserialize(using = String2Long.class)
+    @Generator("snowFlakeGenerator")
+    private Long sysPublishConfigId;
+
+    /**应用代码*/
+    @Column(name = "application_code")
+    private String applicationCode;
+
+    /**创建人所属公司id*/
+    @Column(name = "create_company_id")
+    @JsonSerialize(using = Long2String.class)
+    @JsonDeserialize(using = String2Long.class)
+    private Long createCompanyId;
+
+    /**创建人所属公司name*/
+    @Column(name = "create_company_name")
+    private String createCompanyName;
+
+    /**创建时间*/
+    @Column(name = "create_time")
+    private Date createTime;
+
+    /**创建人id*/
+    @Column(name = "creator_id")
+    @JsonSerialize(using = Long2String.class)
+    @JsonDeserialize(using = String2Long.class)
+    private Long creatorId;
+
+    /**创建人名称*/
+    @Column(name = "creator_name")
+    private String creatorName;
+
+    /**删除标识（0-未删除，1-删除）*/
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+    /**是否开启（0-否，1-是）*/
+    @Column(name = "is_open")
+    private Boolean isOpen;
+
+    /**修改人id*/
+    @Column(name = "modifier_id")
+    @JsonSerialize(using = Long2String.class)
+    @JsonDeserialize(using = String2Long.class)
+    private Long modifierId;
+
+    /**修改人名称*/
+    @Column(name = "modifier_name")
+    private String modifierName;
+
+    /**修改人公司id*/
+    @Column(name = "modify_company_id")
+    @JsonSerialize(using = Long2String.class)
+    @JsonDeserialize(using = String2Long.class)
+    private Long modifyCompanyId;
+
+    /**修改人所属公司name*/
+    @Column(name = "modify_company_name")
+    private String modifyCompanyName;
+
+    /**修改时间*/
+    @Column(name = "modify_time")
+    private Date modifyTime;
+
+    /**版本号*/
+    @Column(name = "record_version")
+    private Integer recordVersion;
+
+    /**时间段*/
+    @Column(name = "times")
+    private String times;
+
+}
