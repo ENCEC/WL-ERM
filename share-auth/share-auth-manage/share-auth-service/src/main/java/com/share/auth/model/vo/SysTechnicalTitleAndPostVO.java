e105df1e83c4271c36cea282883790eaa80b0bb7
weiqi <1821831707@qq.com>
2022-07-26 18:23:54 +0800

    第四次提交岗位职称

---
+++ share-auth/share-auth-manage/share-auth-service/src/main/java/com/share/auth/model/vo/SysTechnicalTitleAndPostVO.java
@@ -0,0 +1,52 @@
+package com.share.auth.model.vo;
+
+
+import lombok.Data;
+
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @ClassName SysTechnicalTitleAndPostVO
+ * @Description TODO
+ * @Author weiq
+ * @Date 2022/7/25 15:41
+ * @Version 1.0
+ **/
+@Data
+public class SysTechnicalTitleAndPostVO implements Serializable {
+    /**岗位职称ID*/
+    private Long technicalTitleId;
+
+    /**创建者*/
+
+    private String createBy;
+
+    /**创建时间*/
+    private Date createTime;
+
+    /**岗位ID*/
+    private Long postId;
+
+    /**备注*/
+    private String remark;
+
+    /**工龄*/
+    private String seniority;
+
+    /**状态（0正常 1停用）*/
+    private String status;
+
+    /**职称名称*/
+    private String technicalName;
+
+    /**更新者*/
+    private String updateBy;
+
+    /**更新时间*/
+    private Date updateTime;
+
+    /**岗位名称*/
+    private String postName;
+}
