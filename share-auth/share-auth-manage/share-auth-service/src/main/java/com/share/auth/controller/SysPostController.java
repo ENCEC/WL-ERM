7c54a3b67654bcef0132467e9ca28f5ec6c61fed
“migit config --global user.email “766417416@qq.com <766417416@qq.com>
2022-07-28 10:00:12 +0800

    根据前端需求修改Page<>字段

--- share-auth/share-auth-manage/share-auth-service/src/main/java/com/share/auth/controller/SysPostController.java
+++ share-auth/share-auth-manage/share-auth-service/src/main/java/com/share/auth/controller/SysPostController.java
@@ -1,46 +1,48 @@
 package com.share.auth.controller;
 
 import com.gillion.ds.client.api.queryobject.model.Page;
-import com.gillion.ec.core.utils.ResultUtils;
 import com.share.auth.domain.SysPostDTO;
-import com.share.auth.domain.UemUserDto;
-import com.share.auth.domain.UemUserEditDTO;
-import com.share.auth.model.entity.SysPost;
 import com.share.auth.service.SysPostService;
-import com.share.support.result.CommonResult;
 import com.share.support.result.ResultHelper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 
 import javax.validation.Valid;
-import java.util.Map;
-import java.util.Objects;
+
 
 /**
  * @author tanjp
  * @Date 2022/7/26 11:46
  */
 @RestController
-@RequestMapping("SysPost")
+@RequestMapping("sysPost")
 @Api(value = "岗位管理控制器")
 public class SysPostController {
     @Autowired
     private SysPostService sysPostService;
 
-    @PostMapping("addSysPost")
-    @ApiOperation(value = "新增岗位")
-    @ApiImplicitParam(name = "sysPost", value = "岗位信息新增修改接口入参", required = true, dataType = "SysPost", paramType = "addSysPost")
-    public Map<String, Object> addSysPost(@RequestBody SysPost sysPost){
-        sysPostService.addSysPost(sysPost);
-        return ResultUtils.getSuccessResultData(true);
+
+    /**
+     * 新增岗位信息
+     *
+     * @param sysPostDTO 岗位信息封装类
+     * @return Page<SysPostDTO>
+     * @author tanjp
+     * @date 2022/7/27
+     */
+    @ApiOperation("岗位新增")
+    @ApiImplicitParam(name = "sysPostDTO", value = "新增接口入参", required = true, dataType = "SysPostDTO")
+    @PostMapping(value = "/saveSysPost")
+    public ResultHelper<?> saveSysPost(@RequestBody @Valid SysPostDTO sysPostDTO){
+        return sysPostService.saveSysPost(sysPostDTO);
     }
+
+
 
     /**
      * 查询岗位信息
@@ -53,9 +55,9 @@ public class SysPostController {
     @ApiOperation("根据岗位名、启禁用状态查询信息")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "postName", value = "岗位名称", dataTypeClass = String.class, paramType = "body"),
-            @ApiImplicitParam(name = "status", value = "启用/禁用状态", dataTypeClass = Boolean.class, paramType = "body")
+            @ApiImplicitParam(name = "status", value = "启用/禁用状态", dataTypeClass = String.class, paramType = "body")
     })
-    @PostMapping("/queryUemUser")
+    @PostMapping("/querySysPost")
     public ResultHelper<Page<SysPostDTO>> querySysPost(@RequestBody SysPostDTO sysPostDTO) {
         return sysPostService.querySysPost(sysPostDTO);
     }
@@ -71,7 +73,7 @@ public class SysPostController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "sysPostId", value = "ID", required = true, dataTypeClass = Long.class, paramType = "body")
     })
-    @GetMapping("/getUemUser")
+    @GetMapping("/getSysPost")
     public ResultHelper<SysPostDTO> getSysPost(@RequestParam Long sysPostId) {
         return sysPostService.getSysPost(sysPostId);
     }
@@ -85,8 +87,8 @@ public class SysPostController {
      */
     @ApiOperation("启用/禁用")
     @ApiImplicitParam(name = "sysPostDTO", value = "信息封装类", required = true, dataType = "SysPostDTO")
-    @PostMapping("/uemUserStartStop")
-    public ResultHelper<?> sysPostStartStop(SysPostDTO sysPostDTO) {
+    @PostMapping("/sysPostStartStop")
+    public ResultHelper<?> sysPostStartStop(@RequestBody @Valid SysPostDTO sysPostDTO) {
         return sysPostService.sysPostStartStop(sysPostDTO);
     }
 
@@ -99,7 +101,7 @@ public class SysPostController {
      * @date 2022/7/27
      */
     @ApiOperation("修改信息")
-    @PostMapping("/editUemUser")
+    @PostMapping("/updateSysPost")
     @ApiImplicitParam(name = "sysPostDTO", value = "信息封装类", required = true, dataType = "SysPostDTO")
     public ResultHelper<?> updatePostStartStop(@RequestBody @Valid SysPostDTO sysPostDTO) {
         return sysPostService.updatePostStartStop(sysPostDTO);
@@ -117,7 +119,7 @@ public class SysPostController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "sysPostId", value = "ID", required = true, dataTypeClass = Long.class, paramType = "body")
     })
-    @PostMapping("/deleteUemUser")
+    @PostMapping("/deleteSysPost")
     public ResultHelper<?> deletePostById(@RequestParam Long sysPostId) {
         return sysPostService.deletePostById(sysPostId);
     }
