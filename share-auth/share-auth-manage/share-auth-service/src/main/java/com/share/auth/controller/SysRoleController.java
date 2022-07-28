07521c0df520582820c06bb0d7ec3e16ab1b5334
xuzt <xuzt@gillion.com.cn>
2022-07-28 17:11:56 +0800

    task#762604  权限管理后端开发

--- share-auth/share-auth-manage/share-auth-service/src/main/java/com/share/auth/controller/SysRoleController.java
+++ share-auth/share-auth-manage/share-auth-service/src/main/java/com/share/auth/controller/SysRoleController.java
@@ -12,6 +12,8 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 /**
  * 部门角色管理
  * @author xuzt <xuzt@gillion.com.cn>
@@ -117,5 +119,17 @@ public class SysRoleController {
     @ApiImplicitParam(name = "sysRoleId", value = "删除角色接口入参", required = true, dataType = "Long")
     public ResultHelper<Object> deleteSysRole(@RequestParam(name = "sysRoleId") Long sysRoleId) {
         throw new UnsupportedOperationException();
+    }
+
+    /**
+     * 获取所有未禁用角色
+     * @return com.share.support.result.ResultHelper<java.util.List<com.share.support.model.Role>>
+     * @author xuzt <xuzt@gillion.com.cn>
+     * @date 2022-07-28
+     */
+    @GetMapping("/queryAllValidRole")
+    @ApiOperation(value = "获取所有未禁用角色")
+    public ResultHelper<List<SysRoleDTO>> queryAllValidRole() {
+        return sysRoleService.queryAllValidRole();
     }
 }
