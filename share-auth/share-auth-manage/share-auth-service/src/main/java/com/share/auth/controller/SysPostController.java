c783370b1aaa66e63cc3747c79d434448fdfc845
tanjp <766417416@qq.com>
2022-07-28 19:57:19 +0800

    根据前端需求修改接口以及对应的格式

--- share-auth/share-auth-manage/share-auth-service/src/main/java/com/share/auth/controller/SysPostController.java
+++ share-auth/share-auth-manage/share-auth-service/src/main/java/com/share/auth/controller/SysPostController.java
@@ -119,7 +119,7 @@ public class SysPostController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "sysPostId", value = "ID", required = true, dataTypeClass = Long.class, paramType = "body")
     })
-    @PostMapping("/deleteSysPost")
+    @GetMapping("/deleteSysPost")
     public ResultHelper<?> deletePostById(@RequestParam Long sysPostId) {
         return sysPostService.deletePostById(sysPostId);
     }
