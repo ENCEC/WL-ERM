4a36aa72ad0321b0d46c4520a91bea614eff4709
tanjp <766417416@qq.com>
2022-08-04 17:54:45 +0800

    修改bug

--- wl-train/wl-train-business/src/main/java/com/gillion/service/impl/StandardDetailServiceImpl.java
+++ wl-train/wl-train-business/src/main/java/com/gillion/service/impl/StandardDetailServiceImpl.java
@@ -12,7 +12,6 @@ import com.gillion.service.StandardDetailService;
 import com.gillion.train.api.model.vo.StandardDetailVO;
 import com.share.support.result.CommonResult;
 import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.BeanUtils;
 import com.share.support.result.ResultHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,7 +26,7 @@ import java.util.Objects;
  * @Date 2022/8/1 13:20
  */
 @Service
-public class StandardDetailServiceImpl implements StandardDetailService  {
+public class StandardDetailServiceImpl implements StandardDetailService {
 
     @Autowired
     private StandardDetailService standardDetailService;
@@ -63,7 +62,7 @@ public class StandardDetailServiceImpl implements StandardDetailService  {
         Page<StandardDetailVO> standardDetailVOPage = DSContext
                 .customization("select_StandardDetail")
                 .select()
-                .paging(currentPage,pageSize)
+                .paging(currentPage, pageSize)
                 .mapperTo(StandardDetailVO.class)
                 .execute(standardDetailVO);
 
@@ -71,7 +70,7 @@ public class StandardDetailServiceImpl implements StandardDetailService  {
     }
 
     /**
-     *新增项目
+     * 新增项目
      *
      * @param standardDetailVO 部门项目信息封装类
      * @return ResultHelper<?>
@@ -93,26 +92,26 @@ public class StandardDetailServiceImpl implements StandardDetailService  {
         //新增的执行序号<= 最大的执行序号
         if (actionSerialNum <= max_actionSerialNum) {
             List<StandardDetail> standardDetailList = QStandardDetail.standardDetail
-                    .select(QStandardDetail.actionSerialNum)
-                    .where(QStandardDetail.actionSerialNum.goe$(max_actionSerialNum))
+                    .select(QStandardDetail.standardDetail.fieldContainer())
+                    .where(QStandardDetail.actionSerialNum.goe$(actionSerialNum))
                     .execute();
-            for (int i = 0;i < standardDetailList.size();i++) {
-                StandardDetail standardDetail1=standardDetailList.get(i);
-                standardDetail1.setActionSerialNum(standardDetailList.get(i).getActionSerialNum()+1);
+            for (int i = 0; i < standardDetailList.size(); i++) {
+                StandardDetail standardDetail1 = standardDetailList.get(i);
+                standardDetail1.setActionSerialNum(standardDetailList.get(i).getActionSerialNum() + 1);
                 standardDetail1.setRowStatus(RowStatusConstants.ROW_STATUS_MODIFIED);
                 QStandardDetail.standardDetail.save(standardDetail1);
             }
         }
         //新增的执行序号大于最大的执行序号，新增执行序号=max+1
         if (actionSerialNum > max_actionSerialNum) {
-            actionSerialNum = max_actionSerialNum+1;
+            actionSerialNum = max_actionSerialNum + 1;
             standardDetailVO.setActionSerialNum(actionSerialNum);
         }
         standardDetailVO.setRowStatus(RowStatusConstants.ROW_STATUS_ADDED);
         standardDetailVO.setCreateTime(new DateTime());
 
 
-        //测试数据   后续调用父表的方面
+        //测试数据   后续调用父表的新增接口
         StandardEntry standardEntry = new StandardEntry();
         standardEntry.setCreateTime(standardDetailVO.getCreateTime());
         standardEntry.setEntryName(standardDetailVO.getEntryName());
@@ -133,7 +132,7 @@ public class StandardDetailServiceImpl implements StandardDetailService  {
     }
 
     /**
-     *删除
+     * 删除
      *
      * @param standardDetailId id
      * @return ResultHelper<?>
@@ -148,30 +147,41 @@ public class StandardDetailServiceImpl implements StandardDetailService  {
         }
         StandardDetail standardDetail = this.getStandardDetailById(standardDetailId);
         if (Objects.isNull(standardDetail)) {
-            return CommonResult.getFaildResultData("岗位信息不存在");
+            return CommonResult.getFaildResultData("信息不存在");
         }
 
 
-       //根据standardDetailId 找到 actionSerialNum
-        int count = QStandardDetail.standardDetail
-                .selectCount()
-                .where(QStandardDetail.standardDetailId.eq$(standardDetailId))
-                .execute();
+        //根据standardDetailId 找到 standardEntryId
+        Long standardEntryId = standardDetail.getStandardEntryId();
+        //序列号
+        Integer serialNum = standardDetail.getActionSerialNum();
 
-        //找出当前比actionSerialNum 大的actionSerialNum
-        List<StandardDetail> standardDetailList = QStandardDetail.standardDetail
-                .select(QStandardDetail.actionSerialNum)
-                .where(QStandardDetail.actionSerialNum.goe$(count))
-                .execute();
+        //获取最大的执行序号
+        Integer max_actionSerialNum = this.maxActionSerialNum();
 
-        //后面的数据递减
-        for (int i = 0;i < standardDetailList.size();i++) {
-            StandardDetail standardDetail1=standardDetailList.get(i);
-            standardDetail1.setActionSerialNum(standardDetailList.get(i).getActionSerialNum()-1);
-            standardDetail1.setRowStatus(RowStatusConstants.ROW_STATUS_MODIFIED);
-            QStandardDetail.standardDetail.save(standardDetail1);
+        if (serialNum < max_actionSerialNum) {
+            //找出当前比actionSerialNum 大的actionSerialNum
+            List<StandardDetail> standardDetailList = QStandardDetail.standardDetail
+                    .select(QStandardDetail.standardDetail.fieldContainer())
+                    .where(QStandardDetail.actionSerialNum.goe$(serialNum))
+                    .execute();
+
+            //后面的数据递减
+            for (int i = 0; i < standardDetailList.size(); i++) {
+                StandardDetail standardDetail1 = standardDetailList.get(i);
+                standardDetail1.setActionSerialNum(standardDetailList.get(i).getActionSerialNum() - 1);
+                standardDetail1.setRowStatus(RowStatusConstants.ROW_STATUS_MODIFIED);
+                QStandardDetail.standardDetail.save(standardDetail1);
+            }
         }
 
+
+        //先删除父表
+        //测试 后续调用父类的删除接口
+        StandardEntry standardEntry = new StandardEntry();
+        standardEntry.setRowStatus(RowStatusConstants.ROW_STATUS_DELETED);
+        standardEntry.setStandardEntryId(standardEntryId);
+        QStandardEntry.standardEntry.deleteById(standardEntry.getStandardEntryId());
 
         standardDetail.setRowStatus(RowStatusConstants.ROW_STATUS_DELETED);
         int row = QStandardDetail.standardDetail.deleteById(standardDetailId);
@@ -193,22 +203,21 @@ public class StandardDetailServiceImpl implements StandardDetailService  {
     @Override
     public ResultHelper<?> standardDetailStartStop(StandardDetailVO standardDetailVO) {
         //id
-        Long standardEntryId = standardDetailVO.getStandardEntryId();
+        Long standardDetailId = standardDetailVO.getStandardDetailId();
         //(0禁用,1启)
         Boolean status = standardDetailVO.getStatus();
         //检查岗位是否存在
-        if (Objects.isNull(standardEntryId)) {
-            return CommonResult.getFaildResultData("岗位ID不能为空");
+        if (Objects.isNull(standardDetailId)) {
+            return CommonResult.getFaildResultData("ID不能为空");
         }
-        StandardDetail standardDetail = this.getStandardDetailById(standardEntryId);
+        StandardDetail standardDetail = this.getStandardDetailById(standardDetailId);
         if (Objects.isNull(standardDetail)) {
-            return CommonResult.getFaildResultData("岗位信息不存在");
+            return CommonResult.getFaildResultData("信息不存在");
         }
-        standardDetail.setStandardEntryId(standardEntryId);
+        standardDetail.setStandardDetailId(standardDetailId);
         standardDetail.setStatus(status);
         standardDetail.setRowStatus(RowStatusConstants.ROW_STATUS_MODIFIED);
         int updateStatus = QStandardDetail.standardDetail.selective(QStandardDetail.status).execute(standardDetail);
-
         //判断是否成功
         if (updateStatus > 0) {
             return CommonResult.getSuccessResultData("启停成功");
@@ -218,7 +227,7 @@ public class StandardDetailServiceImpl implements StandardDetailService  {
     }
 
     /**
-     *修改
+     * 修改
      *
      * @param standardDetailVO
      * @return ResultHelper<?>
@@ -228,57 +237,97 @@ public class StandardDetailServiceImpl implements StandardDetailService  {
     @Override
     public ResultHelper<?> updateStandardDetail(StandardDetailVO standardDetailVO) {
         //根据主键查询岗位信息
-        Long standardEntryId = standardDetailVO.getStandardEntryId();
-        if (Objects.isNull(standardEntryId)) {
+        Long standardDetailId = standardDetailVO.getStandardDetailId();
+        StandardDetail standardDetail = this.getStandardDetailById(standardDetailId);
+        if (Objects.isNull(standardDetailId)) {
             return CommonResult.getFaildResultData("id不允许为空");
         }
-        StandardDetail standardDetail = this.getStandardDetailById(standardEntryId);
-        if (Objects.isNull(standardDetail)) {
+        if (Objects.isNull(standardDetail.getStandardDetailId())) {
             return CommonResult.getFaildResultData("id不存在");
         }
 
-        BeanUtils.copyProperties(standardDetailVO, standardDetail);
+        //判断是否输入的执行序号
+        int actionSerialNum = 0;
+        if (Objects.isNull(standardDetailVO.getActionSerialNum())) {
+            actionSerialNum = standardDetail.getActionSerialNum();
+        } else {
+            actionSerialNum = standardDetailVO.getActionSerialNum();
+        }
 
-        //输入的执行序号
-        int actionSerialNum = standardDetailVO.getActionSerialNum();
+        //原先的执行序号
+        int actionSerial = standardDetail.getActionSerialNum();
         //获取最大的执行序号
         Integer max_actionSerialNum = this.maxActionSerialNum();
-        //判断执行序号是否为null ，赋值1
-        if (actionSerialNum <=0) {
+        //赋值1
+        if (actionSerialNum <= 0) {
             actionSerialNum = 1;
-            standardDetailVO.setActionSerialNum(actionSerialNum);
         }
         //新增的执行序号<= 最大的执行序号
         if (actionSerialNum <= max_actionSerialNum) {
-            List<StandardDetail> standardDetailList = QStandardDetail.standardDetail
-                    .select(QStandardDetail.actionSerialNum)
-                    .where(QStandardDetail.actionSerialNum.goe$(max_actionSerialNum))
-                    .execute();
-            for (int i = 0;i < standardDetailList.size();i++) {
-                StandardDetail standardDetail1=standardDetailList.get(i);
-                standardDetail1.setActionSerialNum(standardDetailList.get(i).getActionSerialNum()+1);
-                standardDetail1.setRowStatus(RowStatusConstants.ROW_STATUS_MODIFIED);
-                QStandardDetail.standardDetail.save(standardDetail1);
+            //前移
+            if (actionSerialNum < standardDetail.getActionSerialNum()) {
+                //前移位置(actionSerialNum) 到  (actionSerial-1)  往后移动一格
+                List<StandardDetail> standardDetailList = QStandardDetail.standardDetail
+                        .select(QStandardDetail.standardDetail.fieldContainer())
+                        .where(QStandardDetail.actionSerialNum.between$(actionSerialNum,actionSerial-1))
+                        .execute();
+                for (int i = 0; i < standardDetailList.size(); i++) {
+                    StandardDetail standardDetail1 = standardDetailList.get(i);
+                    standardDetail1.setActionSerialNum(standardDetailList.get(i).getActionSerialNum() + 1);
+                    standardDetail1.setRowStatus(RowStatusConstants.ROW_STATUS_MODIFIED);
+                    QStandardDetail.standardDetail.save(standardDetail1);
+                }
+            } else {
+                List<StandardDetail> standardDetailList = QStandardDetail.standardDetail
+                        .select(QStandardDetail.standardDetail.fieldContainer())
+                        .where(QStandardDetail.actionSerialNum.between$(actionSerial+1,actionSerialNum))
+                        .execute();
+                for (int i = 0; i < standardDetailList.size(); i++) {
+                    StandardDetail standardDetail1 = standardDetailList.get(i);
+                    standardDetail1.setActionSerialNum(standardDetailList.get(i).getActionSerialNum() - 1);
+                    standardDetail1.setRowStatus(RowStatusConstants.ROW_STATUS_MODIFIED);
+                    QStandardDetail.standardDetail.save(standardDetail1);
+                }
             }
+
         }
         //输入的执行序号大于最大的执行序号，新增执行序号=max+1
         if (actionSerialNum > max_actionSerialNum) {
-            actionSerialNum = max_actionSerialNum+1;
-            standardDetailVO.setActionSerialNum(actionSerialNum);
+            actionSerialNum = max_actionSerialNum + 1;
         }
 
+        standardDetail.setActionSerialNum(actionSerialNum);
+//        BeanUtils.copyProperties(standardDetail, standardDetailVO);
 
-        standardDetail.setRowStatus(RowStatusConstants.ROW_STATUS_MODIFIED);
-        //更新时间
-        standardDetail.setModifyTime(new DateTime());
-        int row = QStandardDetail.standardDetail.save(standardDetail);
 
-        if (row > 1) {
-            return CommonResult.getSuccessResultData("修改成功");
-        } else {
-            return CommonResult.getFaildResultData("修改失败");
+        //先改父表后改子表    (测试  后续调用父表的修改接口)
+        StandardEntry standardEntry = new StandardEntry();
+        //父表id
+        standardEntry.setStandardEntryId(standardDetail.getStandardEntryId());
+        //条目类型
+        standardEntry.setItemType(standardDetailVO.getItemType());
+        //规范条目
+        standardEntry.setEntryName(standardDetailVO.getEntryName());
+        standardEntry.setActionSerialNum(actionSerialNum);
+        standardEntry.setRowStatus(RowStatusConstants.ROW_STATUS_MODIFIED);
+        int action = QStandardEntry.standardEntry.save(standardEntry);
+        if (action > 0) {
+            standardDetail.setRowStatus(RowStatusConstants.ROW_STATUS_MODIFIED);
+            //更新时间
+            standardDetail.setModifyTime(new DateTime());
+            standardDetail.setStatus(standardDetailVO.getStatus());
+            standardDetail.setDetailName(standardDetailVO.getDetailName());
+            int row = QStandardDetail.standardDetail.save(standardDetail);
+
+            if (row > 0) {
+                return CommonResult.getSuccessResultData("子表修改成功");
+            } else {
+                return CommonResult.getFaildResultData("子表修改失败");
+            }
         }
+        return CommonResult.getSuccessResultData("修改成功");
     }
+
 
     /**
      *规范条目种类
@@ -346,6 +395,8 @@ public class StandardDetailServiceImpl implements StandardDetailService  {
             return null;
         }
     }
+
+
     /**
      *最大执行序号
      *
