eaf24cd43b66d30bb03f581398601132075287b7
weizr <544872527@qq.com>
2022-08-17 22:52:31 +0800

    新增我的任务模块服务调用api

--- share-auth/share-auth-manage/share-auth-api/src/main/java/com/share/auth/api/TaskInfoInterface.java
+++ share-auth/share-auth-manage/share-auth-api/src/main/java/com/share/auth/api/TaskInfoInterface.java
@@ -2,6 +2,7 @@ package com.share.auth.api;
 
 import com.share.auth.domain.UemUserDto;
 import com.share.support.result.ResultHelper;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,9 +38,28 @@ public interface TaskInfoInterface {
     /**
      * 服务调用---转正，离职，辞退---查看信息
      *
-     * @author wzr
-     * @date 2022-08-04
+     * @param uemUserId
+     * @date 2022-08-16
      */
     @GetMapping("/uemUserManage/queryStaffInfo")
     UemUserDto queryStaffInfo(@RequestParam(value = "uemUserId") Long uemUserId);
+
+
+    /**
+     * 服务调用---添加离职信息同时生成一个新任务
+     *
+     * @param uemUserDto
+     * @date 2022-08-17
+     */
+    @PostMapping("/uemUserManage/saveResignInfo")
+    ResultHelper<Object> saveResignInfo(@RequestBody UemUserDto uemUserDto);
+
+    /**
+     * 服务调用---添加辞退信息同时生成一个新任务
+     *
+     * @param uemUserDto
+     * @date 2022-08-17
+     */
+    @PostMapping("/uemUserManage/saveDismissInfo")
+    ResultHelper<Object> saveDismissInfo(@RequestBody UemUserDto uemUserDto);
 }
