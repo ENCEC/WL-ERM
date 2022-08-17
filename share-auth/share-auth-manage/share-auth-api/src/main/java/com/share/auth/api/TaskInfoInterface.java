b8165fd6920d444c5c5c65fb7d4403f7248848f9
weiqi <1821831707@qq.com>
2022-08-17 14:54:19 +0800

    新增员工申请表字段，修改上传文件接口

--- share-auth/share-auth-manage/share-auth-api/src/main/java/com/share/auth/api/TaskInfoInterface.java
+++ share-auth/share-auth-manage/share-auth-api/src/main/java/com/share/auth/api/TaskInfoInterface.java
@@ -33,4 +33,13 @@ public interface TaskInfoInterface {
      */
     @RequestMapping ("/uemUserManage/queryUemUserById")
     UemUserDto queryUemUserById(@RequestParam(value = "uemUserId") Long uemUserId);
+
+    /**
+     * 服务调用---转正，离职，辞退---查看信息
+     *
+     * @author wzr
+     * @date 2022-08-04
+     */
+    @GetMapping("/uemUserManage/queryStaffInfo")
+    UemUserDto queryStaffInfo(@RequestParam(value = "uemUserId") Long uemUserId);
 }
